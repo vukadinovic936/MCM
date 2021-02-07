649768837cb77187ca20f66d3c543b119caec560
vukadinovic936 <milosvuk99@gmail.com>
2021-02-06 22:03:24 -0700

    changes

--- data.jl
+++ data.jl
@@ -147,23 +147,6 @@ HR_data = [	[0.30, 0.36, 0.34],
 		[1.92, 3.37, 5.67],
 		[0.74, 1.00, 1.04]]
 
-# ╔═╡ 62ff5ea8-6765-11eb-1ae9-e597834764df
-geometric_mean(x) = (x[1]*x[2]*x[3])^(1/3)
-
-# ╔═╡ a2efe0d2-6765-11eb-2249-c50e7ca75cd1
-HR = geometric_mean.(HR_data)
-
-# ╔═╡ 19726662-6761-11eb-1d7a-df42ac99ef1c
-begin
-	using DataFrames
-	using CSV
-	using Plots
-	df = DataFrame(Species = species,
-				   decomposition_rate = DR,
-				   growth_rate = HR)
-	#CSV.write("/data/MCM/dc_data.csv",df)
-end
-
 # ╔═╡ c4826416-6767-11eb-0e61-e912d529bebc
 md" Clear up why are we using geometric mean  \
 	Generate a table of our data
@@ -172,54 +155,11 @@ md" Clear up why are we using geometric mean  \
 # ╔═╡ 54503b68-6768-11eb-369c-9d1714c8a21c
 md" Now let's plot stuff "
 
-# ╔═╡ 70cbb1c8-6768-11eb-36c4-b7f1e44ceb9d
-scatter(HR, DR, 
-		xaxis = "Hyphal Extension Rate", 
-		yaxis="Decomposition Rate", 
-		legend = false)
-
 # ╔═╡ 67f36fa8-676d-11eb-3055-bd1f93a5c936
 scatter(log.(DR))
 
-# ╔═╡ 452e74e2-6772-11eb-347f-3556436653ca
-df2 = CSV.File("Fungal_trait_data.csv") |> DataFrame
-
 # ╔═╡ 7a738940-6776-11eb-39e7-c33e9301faf3
 normalize(x) = (x.-minimum(x))/(maximum(x)-minimum(x))
-
-# ╔═╡ a1719b38-6774-11eb-01fd-511391d46d16
-begin
-	data = df2["water.niche.width"][1:34]
-	moisture_niche_width= normalize(data)
-end
-
-# ╔═╡ aadd29bc-6774-11eb-02c6-59f021e86c6a
-competitive_ranking = normalize(df2["ranking"][1:34])
-
-# ╔═╡ 35dfd5e6-6775-11eb-23d6-052efdcd1dd1
-moisture_tolerance = (competitive_ranking - moisture_niche_width)
-
-# ╔═╡ 8cea6466-6836-11eb-2704-797ca64396a1
-begin
-	df3 = DataFrame(Species = species,
-				   decomposition_rate = DR,
-				   growth_rate = HR,
-				   moisture_tolerance = moisture_tolerance)
-	CSV.write("/data/MCM/dc_data.csv",df)
-end
-
-# ╔═╡ c7ecc5ca-6775-11eb-0da6-09045374100c
-scatter(moisture_tolerance, log.(DR), legend=false)
-
-# ╔═╡ 7af43442-677a-11eb-2a4f-71ca69516b35
-begin
-	df_final = DataFrame(Species = species,
-				   decomposition_rate = DR,
-				   growth_rate = HR, 
-				   moisture_tolerance = moisture_tolerance)
-
-	CSV.write("/data/MCM/dc_data2.csv",df_final)
-end
 
 # ╔═╡ 4c2ed13a-6851-11eb-3fcb-c79d02727c89
 md"
@@ -238,11 +178,82 @@ begin
 	(+)(p1::Point, p2::Point) = Point(p1.x+p2.x, p1.y+p2.y)
 end
 
+# ╔═╡ 6892328c-68ff-11eb-2a85-4f5aa9d53b32
+begin
+	import Base.*
+	(*)(c::Float64, p::Point) = Point(p.x*c, p.y*c)
+end
+
+# ╔═╡ 62ff5ea8-6765-11eb-1ae9-e597834764df
+geometric_mean(x) = (x[1]*x[2]*x[3])^(1/3)
+
+# ╔═╡ a2efe0d2-6765-11eb-2249-c50e7ca75cd1
+HR = geometric_mean.(HR_data)
+
+# ╔═╡ 19726662-6761-11eb-1d7a-df42ac99ef1c
+begin
+	using DataFrames
+	using CSV
+	using Plots
+	df = DataFrame(Species = species,
+				   decomposition_rate = DR,
+				   growth_rate = HR)
+	#CSV.write("/data/MCM/dc_data.csv",df)
+end
+
+# ╔═╡ 452e74e2-6772-11eb-347f-3556436653ca
+df2 = CSV.File("Fungal_trait_data.csv") |> DataFrame
+
+# ╔═╡ a1719b38-6774-11eb-01fd-511391d46d16
+begin
+	data = df2["water.niche.width"][1:34]
+	moisture_niche_width= normalize(data)
+end
+
+# ╔═╡ aadd29bc-6774-11eb-02c6-59f021e86c6a
+competitive_ranking = normalize(df2["ranking"][1:34])
+
+# ╔═╡ 35dfd5e6-6775-11eb-23d6-052efdcd1dd1
+moisture_tolerance = (competitive_ranking - moisture_niche_width)
+
+# ╔═╡ c7ecc5ca-6775-11eb-0da6-09045374100c
+scatter(moisture_tolerance, log.(DR), legend=false)
+
+# ╔═╡ 70cbb1c8-6768-11eb-36c4-b7f1e44ceb9d
+scatter(HR, DR, 
+		xaxis = "Hyphal Extension Rate", 
+		yaxis="Decomposition Rate", 
+		legend = false)
+
+# ╔═╡ 8cea6466-6836-11eb-2704-797ca64396a1
+begin
+	df3 = DataFrame(Species = species,
+				   decomposition_rate = DR,
+				   growth_rate = HR,
+				   moisture_tolerance = moisture_tolerance)
+	CSV.write("/data/MCM/dc_data.csv",df)
+end
+
+# ╔═╡ 7af43442-677a-11eb-2a4f-71ca69516b35
+begin
+	df_final = DataFrame(Species = species,
+				   decomposition_rate = DR,
+				   growth_rate = HR, 
+				   moisture_tolerance = moisture_tolerance)
+
+	CSV.write("/data/MCM/dc_data2.csv",df_final)
+end
+
 # ╔═╡ e4efbe66-677a-11eb-105b-819ebbd28990
 scatter(HR, DR, markersize=(moisture_tolerance.+1)*5, alpha=0.5,
 		xaxis = "Hyphal Extension Rate", 
 		yaxis="Decomposition Rate", 
 		legend = false)
+
+# ╔═╡ ed19a464-68ea-11eb-1d13-cd2480f4186c
+function isequal(p1::Point,p2::Point)
+	return p1.x== p2.x && p1.y==p2.y
+end
 
 # ╔═╡ 1ea0c1a8-683d-11eb-0efa-89c527035050
 struct Hypha
@@ -289,14 +300,14 @@ end
 					filter(row -> row["Species"] .== species, df3)["growth_rate"][1],
 					filter(row -> row["Species"] .== species, df3)["moisture_tolerance"][1],
 					[center,center,center,center,center,center,center,center],
-					[Point(1,0)+center,
-					 Point(sqrt(2)/2,sqrt(2)/2)+center,
-					 Point(-1,0)+center,
-					 Point(-sqrt(2)/2,sqrt(2)/2)+center,
-					 Point(0,1)+center,
-					 Point(-sqrt(2)/2,-sqrt(2)/2)+center,
-					 Point(0,-1)+center,
-					 Point(sqrt(2)/2,-sqrt(2)/2)+center,],
+					[1e-4*Point(1,0)+center,
+					 1e-4*Point(sqrt(2)/2,sqrt(2)/2)+center,
+					 1e-4*Point(-1,0)+center,
+					 1e-4*Point(-sqrt(2)/2,sqrt(2)/2)+center,
+					 1e-4*Point(0,1)+center,
+					 1e-4*Point(-sqrt(2)/2,-sqrt(2)/2)+center,
+					 1e-4*Point(0,-1)+center,
+					 1e-4*Point(sqrt(2)/2,-sqrt(2)/2)+center,],
 					[0,
 					 pi/4,
 					 pi/2,
@@ -313,14 +324,14 @@ end
 					  true,
 					  true,
 					  true],
-					 [ 1.0
-					   1.0
-			    	   1.0
-					   1.0
-					   1.0
-					   1.0
-					   1.0
-					   1.0]
+					 [ 10^(-5)
+					   10^(-5)
+			    	   10^(-5)
+					   10^(-5)
+					   10^(-5)
+					   10^(-5)
+					   10^(-5)
+					   10^(-5)]
 					 )
 end
 
@@ -353,6 +364,20 @@ md"
 #   JUSTIFYYYYYYYYYYY 
 "
 
+# ╔═╡ b9127852-68f5-11eb-0b11-172f8bb57677
+t = 0.01 # days
+
+# ╔═╡ 7cc884ac-68fc-11eb-0500-b3e0622146dc
+Se = zeros(10000,10000)
+
+# ╔═╡ 469a53c2-68fe-11eb-1064-8d806c382970
+
+
+# ╔═╡ 7651344a-68f5-11eb-17be-d7573461d1a8
+function growth_rate()
+	return 0.5
+end
+
 # ╔═╡ ac8dd314-6847-11eb-3f2e-1163d16252d6
 function next_point(p::Point, ϕ::Float64, isTip::Bool, Si::Float64)
 	
@@ -360,21 +385,26 @@ function next_point(p::Point, ϕ::Float64, isTip::Bool, Si::Float64)
 		return -1
 	end
 	
-	if(bernoulli(0.20))
+	if(Si < 10^(-12)) 
+		return 1
+	end
+	
+
+	if(bernoulli(1/8))
 		if(bernoulli(0.50))
 			ϕ += π/6
 		else
 			ϕ -= π/6	
 		end
 	end
-	
-	return (p,Point(p.x + cos(ϕ), p.y +  sin(ϕ)),ϕ)
+	v = growth_rate() 
+	return (p, Point(p.x + v*t*cos(ϕ), p.y +  v*t*sin(ϕ)),ϕ)
 		
 end
 
 # ╔═╡ e9e11d3c-6853-11eb-0b2f-c310a84e7f9e
 function next_branch(p::Point)
-	d=Normal(pi/4, pi/12)
+	d=Normal(1.4,0.17)
 	ϕ = rand(d)
 	return Point(p.x + cos(ϕ), p.y +  sin(ϕ))
 end
@@ -398,9 +428,6 @@ end
 function getThird(t::Tuple{Point,Point,Float64})
 	return t[3]
 end
-
-# ╔═╡ 3b66cc12-68bb-11eb-1667-dda4d3bc36fb
-getFirst.(next_point.(genk.to,genk.angle,genk.isTip,genk.Si))
 
 # ╔═╡ c097e286-68b9-11eb-2799-333f9b0cd757
 genk
@@ -448,12 +475,28 @@ function intersects(p1, q1, p2, q2)
     o2 = orientation(p1, q1, q2) 
     o3 = orientation(p2, q2, p1) 
     o4 = orientation(p2, q2, q1) 
-  
+  	if(isequal(p1,p2) || isequal(p1,q2) || isequal(q1,p2) || isequal(q1,q2))
+			return false
+	end
     if ((o1 != o2) && (o3 != o4))
         return true
 	end
+    if (o1 == 0 && onSegment(p1, p2, q1))
+		return true
+	end
   
-    return false
+    if (o2 == 0 && onSegment(p1, q2, q1))
+		return true
+	end
+  
+    if (o3 == 0 && onSegment(p2, p1, q2))
+		return true
+	end
+  
+    if (o4 == 0 && onSegment(p2, q1, q2))
+		return true; 
+	end
+	return false;
 end
 
 # ╔═╡ 1f3f2ac8-68d6-11eb-0ae6-972dd9115a49
@@ -504,21 +547,106 @@ function branch_angle()
 	return rand(Normal(1.39626, 0.174533))
 end
 
-# ╔═╡ df7fd6dc-683d-11eb-2a9a-9feba787dd47
-function step!(fungi::Fungi)
-	
-	s = next_point.(fungi.to,fungi.angle,fungi.isTip,fungi.Si)
-	s = filter(x -> x != -1, s)
-		
-	for p in fungi.from
-		if (bernoulli(0.001))
-			ϕ = branch_angle()
-			push!(s, (p, Point(p.x + cos(ϕ), p.y +  sin(ϕ)), ϕ))
+# ╔═╡ d2bd7028-68f4-11eb-3fa0-a77fe1160848
+function getI(c::CartesianIndex{2})
+	return c[2]
+end
+
+# ╔═╡ 6e220a2a-68f4-11eb-2a32-17b9d6b1fef2
+begin
+ar = [-1 -1 1 1 -1]
+getI.(findall(x -> x == 1,ar))
+end
+
+# ╔═╡ d41d5e40-68f6-11eb-0274-1ddf21a46161
+function branch(p::Point, Si::Float64)
+	if Si < 10^(-11)
+		return false
+	else
+		if(bernoulli(Si*t*10^3))
+			return true
 		end
 	end
+	return false
+end
+
+# ╔═╡ eddde038-68f7-11eb-3368-6760f8a0763b
+function shares_endpoint(p1,p2,x1,x2)
+	if ((isequal(p1,x1) && isequal(p2,x2)) || (isequal(p1,x2) && isequal(p2,x1)))
+			return false
+	end
+	return (isequal(p1,x1)
+		|| isequal(p1,x2) 
+		|| isequal(p2,x1) 
+		|| isequal(p2,x2)) 
 	
-	not= "not intersects"
-	## intersection
+end
+
+# ╔═╡ df7fd6dc-683d-11eb-2a9a-9feba787dd47
+function step!(fungi::Fungi)
+	# TIP EXTENSION
+	s = next_point.(fungi.to,fungi.angle,fungi.isTip,fungi.Si)
+	fungi.isTip = fungi.isTip .* 0
+	s = filter(x -> x != -1, s)
+	indices = []
+	
+	try
+	append!(indices, getI.(findall(x -> x==1,s)))
+	catch
+		@warn "blabla"
+	end
+	s = filter(x -> x != 1, s)
+	fungi.isTip[indices] .= 1
+	
+	
+	# BRANCHING
+#	for i in 1:length(fungi.from)
+#		if(branch(fungi.from[i], fungi.Si[i]))
+#			ϕ = branch_angle()
+#			p = fungi.from[i]
+#			
+#			if length(s) == 0
+#				s= [(p, Point(p.x + cos(ϕ), p.y +  sin(ϕ)), ϕ)]
+#			else	
+#				push!(s, (p, Point(p.x + cos(ϕ), p.y +  sin(ϕ)), ϕ))
+#			end
+#			
+#		end
+#	end
+	
+	# TRANSLOCATION
+	dif = zeros(size(fungi.from))
+	for i in 1:length(fungi.from)
+		for j in 1:(length(fungi.from))
+			if(shares_endpoint(fungi.from[i],fungi.to[i],fungi.from[j],fungi.to[j]))
+				dif[i] +=  (fungi.Si[j] - fungi.Si[i])/(1e-4)
+			end
+		end
+	end
+	dif.*=t
+	fungi.Si += dif
+	## change the substrate!
+	
+	# UPTAKE
+	for i in 1:length(fungi.from)
+		xcoord = Int( floor(getx(fungi.to[i])/1e-4))
+		ycoord = Int(floor(gety(fungi.to[i])/1e-4))
+		ΔSi = 600*t*(fungi.Si[i]/(4*1e-9+fungi.Si[i]))
+			 # Se[xcoord, ycoord ]
+		#if ΔSi > Se[xcoord, ycoord]
+			#fungi.Si[i] += Se[xcoord, ycoord]
+			#Se[xcoord, ycoord] = 0
+		#else
+		#	fungi.Si[i] += ΔSi
+			#Se[xcoord, ycoord] -= ΔSi
+		#end
+	end
+	
+	
+	
+	## INTERSECTION
+	new_tips = ones(length(s))
+	
 	for i in 1:length(s)
 		for j in 1:(length(fungi.to))
 			p1 = s[i][1]
@@ -526,10 +654,10 @@ function step!(fungi::Fungi)
 			x1 = fungi.from[j]
 			x2 = fungi.to[j]
 			if(intersects(p1,p2,x1,x2))
-				not="INTERSECT" * string(p1) * " " string(p2) * " " string(x1) * " " string(x2)
 				new_p = inter_point(p1,p2,x1,x2)
-				#s[i][2].x = new_p.x
-				#s[i][2].y = new_p.y
+				s[i][2].x = new_p.x
+				s[i][2].y = new_p.y
+				new_tips[i]=0
 				break
 			end
 		end
@@ -538,29 +666,10 @@ function step!(fungi::Fungi)
 	append!(fungi.from, getFirst.(s))
 	append!(fungi.to, getSecond.(s))
 	append!(fungi.angle, getThird.(s))
-	fungi.isTip = fungi.isTip .*0
-	append!(fungi.isTip, ones(size(s)))
-	append!(fungi.Si, ones(Float64,size(s)))
+	append!(fungi.isTip,new_tips )
+	append!(fungi.Si, zeros(Float64,size(s)))
 	
-	return not
-	#append!(fungi.Si, fungi.Si)
-	#append!(fungi.from, fungi.to)
-	#append!(fungi.to, next_point.(fungi.to))
-#	for i in 1:length(fungi.hyphae)
-#		# each makes a step of growth
-#		push!(fungi.hyphae[i].points,
-#				next_point(fungi.hyphae[i].points[length(fungi.hyphae[i].points)],
-#						   fungi.hyphae[i].points[length(fungi.hyphae[i].points)-1],
-#							fungi))
-#		# branch
-#		if bernoulli(0.1)
-#			push!(fungi.hyphae,
-#				  Hypha(vcat(fungi.hyphae[i].points,
-#				next_branch(fungi.hyphae[i].points[length(fungi.hyphae[i].points)])),
-#						true))
-#		# interesect
-#		end
-#	end
+	return fungi
 end
 
 # ╔═╡ 96846800-6845-11eb-0a66-a7fb85b055bf
@@ -572,13 +681,10 @@ end
 genk
 
 # ╔═╡ 400c487e-68d8-11eb-10f8-f719cf876699
-intersects(Point(62.3923,60.1962),Point(61.8923,61.0622),Point(0,61.001),Point(100,61.001))
+intersects(Point(48.0,51.0),Point(47.13,51.5),Point(49.0,51.0),Point(48.0,51.00))
 
-# ╔═╡ 77f4c4ba-6843-11eb-3920-636754a1d827
-begin
-	step!(genk)
-	#@bind time Slider(1:length(getpoints(genk.hyphae[1])),default=1)
-end
+# ╔═╡ 791b8654-68ea-11eb-3483-0bf062d2508b
+scatter([1],[1])
 
 
 # ╔═╡ 01022fa4-68bf-11eb-10c4-c5c8049ff714
@@ -597,9 +703,18 @@ function plot_fungi(fungi::Fungi)
 	return sc
 end
 
-# ╔═╡ d9d7474c-68d3-11eb-292a-c5a466ab3e4b
-plot_fungi(genk)
+# ╔═╡ 77f4c4ba-6843-11eb-3920-636754a1d827
+begin
+	step!(genk)
+	##@bind time Slider(1:length(getpoints(genk.hyphae[1])),default=1)
+	plot_fungi(genk)
+end
 
+
+# ╔═╡ d9d7474c-68d3-11eb-292a-c5a466ab3e4b
+begin
+	plot_fungi(genk)
+end
 
 # ╔═╡ dea5193e-68c4-11eb-2a88-2b2c0ffca2e2
 plot_fungi(simulation[i])
@@ -644,7 +759,9 @@ Branching angles have been shownto be normally distributed in fungal mycelia (Hu
 # ╟─e4efbe66-677a-11eb-105b-819ebbd28990
 # ╠═4c2ed13a-6851-11eb-3fcb-c79d02727c89
 # ╠═78c91270-6827-11eb-250a-6f5be3f36820
-# ╟─ec175d6c-6828-11eb-2f6f-c75c5b18c3ef
+# ╠═ec175d6c-6828-11eb-2f6f-c75c5b18c3ef
+# ╠═6892328c-68ff-11eb-2a85-4f5aa9d53b32
+# ╠═ed19a464-68ea-11eb-1d13-cd2480f4186c
 # ╠═1ea0c1a8-683d-11eb-0efa-89c527035050
 # ╠═1c533af0-6829-11eb-2f2d-ab894ae524ca
 # ╠═4d04ef40-68b0-11eb-2781-c9d566b2ece5
@@ -654,15 +771,18 @@ Branching angles have been shownto be normally distributed in fungal mycelia (Hu
 # ╟─df20ac7a-6830-11eb-077f-5d0b553551c2
 # ╟─f6aea732-6830-11eb-1fbe-bfd1f16815a3
 # ╠═01e89ad2-684e-11eb-1a44-fde170bc69d3
-# ╟─8babf7d4-684e-11eb-1cc5-8d13e27b32b3
+# ╠═8babf7d4-684e-11eb-1cc5-8d13e27b32b3
+# ╠═b9127852-68f5-11eb-0b11-172f8bb57677
+# ╠═7cc884ac-68fc-11eb-0500-b3e0622146dc
+# ╠═469a53c2-68fe-11eb-1064-8d806c382970
+# ╟─7651344a-68f5-11eb-17be-d7573461d1a8
 # ╠═ac8dd314-6847-11eb-3f2e-1163d16252d6
 # ╠═398fdf3a-6854-11eb-3041-b9130d132483
-# ╟─e9e11d3c-6853-11eb-0b2f-c310a84e7f9e
-# ╟─f3df1314-68b4-11eb-2d92-6b515469e676
+# ╠═e9e11d3c-6853-11eb-0b2f-c310a84e7f9e
+# ╠═f3df1314-68b4-11eb-2d92-6b515469e676
 # ╟─d5796fb4-68b9-11eb-021a-e537d93880d7
 # ╟─f6034e94-68b9-11eb-3586-d18d276bd9be
 # ╟─331a2554-68bb-11eb-0fb9-99fc2c426c1a
-# ╠═3b66cc12-68bb-11eb-1667-dda4d3bc36fb
 # ╠═c097e286-68b9-11eb-2799-333f9b0cd757
 # ╠═d6eac3f2-68be-11eb-026d-6729ab9283b1
 # ╠═1c596458-68c8-11eb-3993-29610d466d9a
@@ -677,6 +797,10 @@ Branching angles have been shownto be normally distributed in fungal mycelia (Hu
 # ╠═aaa7cfd4-68cc-11eb-2f52-c9bf76acd4df
 # ╠═1b972c50-68d0-11eb-07b0-21c499061810
 # ╠═d94caad6-68df-11eb-28f6-c1df93195446
+# ╠═d2bd7028-68f4-11eb-3fa0-a77fe1160848
+# ╠═6e220a2a-68f4-11eb-2a32-17b9d6b1fef2
+# ╠═d41d5e40-68f6-11eb-0274-1ddf21a46161
+# ╠═eddde038-68f7-11eb-3368-6760f8a0763b
 # ╠═df7fd6dc-683d-11eb-2a9a-9feba787dd47
 # ╟─96846800-6845-11eb-0a66-a7fb85b055bf
 # ╟─3a5fc9dc-68ab-11eb-3a40-9fc97f9b2ebe
@@ -684,6 +808,7 @@ Branching angles have been shownto be normally distributed in fungal mycelia (Hu
 # ╠═400c487e-68d8-11eb-10f8-f719cf876699
 # ╠═77f4c4ba-6843-11eb-3920-636754a1d827
 # ╠═d9d7474c-68d3-11eb-292a-c5a466ab3e4b
+# ╠═791b8654-68ea-11eb-3483-0bf062d2508b
 # ╠═01022fa4-68bf-11eb-10c4-c5c8049ff714
 # ╠═eaefdc8c-68a7-11eb-1cd5-575c2445e9ce
 # ╠═dea5193e-68c4-11eb-2a88-2b2c0ffca2e2
